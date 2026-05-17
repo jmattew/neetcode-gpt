819e50a658184d48782cc7357751a2a6b65b8a3e
jmattew <51306296+jmattew@users.noreply.github.com>
2026-05-17 15:26:39 -0700

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -9,26 +9,23 @@ class Solution:
         # y_pred: predicted probabilities
         # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
         # return round(your_answer, 4)
-        x = 0
+        loss = 0
+        
         for i in range(len(y_true)):
-            y_pred[i] = y_pred[i] + 0.0000001
-            x = x + (y_true[i]*np.log(y_pred[i])) + ((1-y_true[i])*np.log(1-y_pred[i]))
-        x = (x/len(y_true)) * -1
-        x = np.round(x,4)
-        return x
-
+            epsilon = y_pred[i] + 0.0000001
+            loss = loss + ((y_true[i] * np.log(epsilon)) + ((1-y_true[i]) * (np.log(1-epsilon))))
+        loss = (loss/len(y_true)) * -1
+        return np.round(loss, 4)
 
     def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
         # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
         # y_pred: predicted probabilities (shape: n_samples x n_classes)
         # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
         # return round(your_answer, 4)
-        x = 0
+        loss = 0
+        epsilon = 0.0000001
         for i in range(len(y_true)):
             for j in range(len(y_true[0])):
-                y_pred[i][j] = y_pred[i][j] + 0.0000001
-                x = x + (y_true[i][j] * np.log(y_pred[i][j]))
-        x = (x/len(y_true)) * -1
-        x = np.round(x,4)
-        return x
-        
+                loss = loss + (y_true[i][j]) * np.log(y_pred[i][j]+epsilon)
+        loss = (loss/len(y_true)) * -1
+        return np.round(loss, 4)
