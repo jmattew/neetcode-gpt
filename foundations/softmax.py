d443a1622cb08977ccd355f4db623e442f1691f1
jmattew <51306296+jmattew@users.noreply.github.com>
2026-05-17 14:53:41 -0700

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -8,11 +8,8 @@ class Solution:
         # z is a 1D NumPy array of logits
         # Hint: subtract max(z) for numerical stability before computing exp
         # return np.round(your_answer, 4)
-        x = 0
-        y = max(z)
-        for j in range(len(z)):
-            x = x + np.exp(z[j]-y)
+        maxim = max(z)
+        y = np.exp(z-maxim)
         for i in range(len(z)):
-            z[i] = np.exp(z[i]-y)/x
-            z[i] = np.round(z[i],4)
-        return z
+            z[i] = np.exp(z[i]-maxim)/sum(y)
+        return np.round(z,4)
